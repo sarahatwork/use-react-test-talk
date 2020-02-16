bcacad518ca3cd88b891d3ba36a023950d6716bb
Sarah Mogin <mogin@work.co>
2020-02-16 17:27:46 -0500

    Update enzyme test to use class + text

--- src/components/Switch/Switch.enzyme.test.js
+++ src/components/Switch/Switch.enzyme.test.js
@@ -5,12 +5,12 @@ import Switch from './Switch';
 describe('Switch test in Enzyme', () => {
   it('is off by default', () => {
     const wrapper = shallow(<Switch />);
-    expect(wrapper.state().isOn).toBe(false);
+    expect(wrapper.find('.text').text()).toContain('Off');
   });
 
   it('switches from off to on on user click', () => {
     const wrapper = shallow(<Switch />);
     wrapper.find('button').simulate('click');
-    expect(wrapper.state().isOn).toBe(true);
+    expect(wrapper.find('.text').text()).toContain('On');
   });
 });
