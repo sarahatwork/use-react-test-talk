094f196a5caf1c57760411850577ae7c251a741e
Sarah Mogin <mogin@work.co>
2020-02-16 21:12:07 -0500

    Revert "Switch to inline snapshots"
    This reverts commit a258a3e0dee0e5ce1484e7b83377766acd447317.

--- src/components/Switch/Switch.snapshot.test.js
+++ src/components/Switch/Switch.snapshot.test.js
@@ -5,38 +5,12 @@ import Switch from './Switch';
 describe('Switch test with Jest snapshot', () => {
   it('is off by default', () => {
     const { container } = render(<Switch />);
-    expect(container.firstChild).toMatchInlineSnapshot(`
-      <div>
-        <span
-          class="text"
-          data-testid="label"
-        >
-          The toggle is: 
-          Off
-        </span>
-        <button>
-          Toggle
-        </button>
-      </div>
-    `);
+    expect(container.firstChild).toMatchSnapshot();
   });
 
   it('switches from off to on on user click', () => {
     const { container, getByRole } = render(<Switch />);
     fireEvent.click(getByRole('button'));
-    expect(container.firstChild).toMatchInlineSnapshot(`
-      <div>
-        <span
-          class="text"
-          data-testid="label"
-        >
-          The toggle is: 
-          On
-        </span>
-        <button>
-          Toggle
-        </button>
-      </div>
-    `);
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
