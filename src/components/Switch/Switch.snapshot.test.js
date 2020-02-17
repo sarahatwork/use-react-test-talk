bdf860f6d6f72e9de3f4efe58bbd3de29b271a70
Sarah Mogin <mogin@work.co>
2020-02-16 21:12:02 -0500

    Switch to inline snapshots

--- src/components/Switch/Switch.snapshot.test.js
+++ src/components/Switch/Switch.snapshot.test.js
@@ -5,12 +5,38 @@ import Switch from './Switch';
 describe('Switch test with Jest snapshot', () => {
   it('is off by default', () => {
     const { container } = render(<Switch />);
-    expect(container.firstChild).toMatchSnapshot();
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      <div>
+        <span
+          class="text"
+          data-testid="label"
+        >
+          The toggle is: 
+          Off
+        </span>
+        <button>
+          Toggle
+        </button>
+      </div>
+    `);
   });
 
   it('switches from off to on on user click', () => {
     const { container, getByRole } = render(<Switch />);
     fireEvent.click(getByRole('button'));
-    expect(container.firstChild).toMatchSnapshot();
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      <div>
+        <span
+          class="text"
+          data-testid="label"
+        >
+          The toggle is: 
+          On
+        </span>
+        <button>
+          Toggle
+        </button>
+      </div>
+    `);
   });
 });
