0e751bd8c3067053eb9c54e68b139e2f96214abb
Sarah Mogin <mogin@work.co>
2020-02-16 20:39:06 -0500

    Chihuahua functionality

--- src/components/Switch/Switch.jsx
+++ src/components/Switch/Switch.jsx
@@ -1,11 +1,27 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import './Switch.css';
 
 export default function Switch() {
   const [isOn, setIsOn] = useState(false);
+  const [dogImage, setDogImage] = useState();
 
-  function toggleSwitch() {
-    setIsOn(currentValue => !currentValue);
-  }
+  useEffect(() => {
+    const fetchDogPic = async () => {
+      const res = await fetch(
+        'https://dog.ceo/api/breed/chihuahua/images/random'
+      );
+      const out = await res.json();
+      setDogImage(out.message);
+    };
+    if (isOn) {
+      fetchDogPic();
+    }
+  }, [isOn]);
+
+  const toggleSwitch = useCallback(() => {
+    setIsOn(!isOn);
+    setDogImage(undefined);
+  }, [isOn]);
 
   return (
     <div>
@@ -13,6 +29,12 @@ export default function Switch() {
         The toggle is: {isOn ? 'On' : 'Off'}
       </span>
       <button onClick={toggleSwitch}>Toggle</button>
+      {isOn &&
+        (dogImage ? (
+          <img className="image" src={dogImage} alt="Chihuahua" />
+        ) : (
+          <span className="text">Loading...</span>
+        ))}
     </div>
   );
 }
